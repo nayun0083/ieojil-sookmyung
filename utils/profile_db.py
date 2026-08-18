0a4e7c7307bb38512582f138a186bfdbadbcdbf3
nayun0083 <luzdelsolnayun@gmail.com>
2026-08-18 13:18:21 +0900

    Modify profile functions to use optional client
    Refactor upsert_profile and get_profile functions to accept an optional client parameter.

--- utils/profile_db.py
+++ utils/profile_db.py
@@ -1,11 +1,13 @@
 from utils.supabase_client import get_client
 
 
-def upsert_profile(user_id: str, email: str, name: str, dept: str, grade: str):
+def upsert_profile(user_id: str, email: str, name: str, dept: str, grade: str, sb=None):
     """
     profiles 테이블에 회원 기본 정보를 저장하거나 수정한다.
+    sb가 전달되면 그 로그인된 client를 사용한다.
     """
-    sb = get_client()
+    if sb is None:
+        sb = get_client()
 
     data = {
         "id": user_id,
@@ -21,17 +23,16 @@ def upsert_profile(user_id: str, email: str, name: str, dept: str, grade: str):
         .execute()
     )
 
-    if res.data:
-        return res.data[0]
-
-    return data
+    return res.data[0] if res.data else data
 
 
-def get_profile(user_id: str):
+def get_profile(user_id: str, sb=None):
     """
     user_id 기준으로 profiles 테이블에서 내 정보를 가져온다.
+    sb가 전달되면 그 로그인된 client를 사용한다.
     """
-    sb = get_client()
+    if sb is None:
+        sb = get_client()
 
     res = (
         sb.table("profiles")
