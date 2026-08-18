56f5d86fdd1832433b3d27e9dd2e8ad012e2888b
simyebin1 <syb9134@sookmyung.ac.kr>
2026-08-18 12:38:10 +0900

    Update match_db.py

--- utils/match_db.py
+++ utils/match_db.py
@@ -1,1 +1,32 @@
+from utils.supabase_client import get_client
 
+
+def create_match_request(
+    mentor_id: str,
+    mentee_id: str,
+    mentor_profile_id: str,
+    topic: str,
+    preferred_field: str,
+    preferred_time: str,
+    question: str,
+):
+    """멘티가 멘토에게 매칭 신청"""
+
+    sb = get_client()
+
+    res = (
+        sb.table("matches")
+        .insert({
+            "mentor_id": mentor_id,
+            "mentee_id": mentee_id,
+            "mentor_profile_id": mentor_profile_id,
+            "topic": topic,
+            "preferred_field": preferred_field,
+            "preferred_time": preferred_time,
+            "question": question,
+            "status": "pending",
+        })
+        .execute()
+    )
+
+    return res.data[0]
