1b0c8f9ef7050a212dcd91e339f7d2f502d84a3f
nayun0083 <luzdelsolnayun@gmail.com>
2026-08-25 17:42:26 +0900

    Update footer text and copyright information

--- components/footer.py
+++ components/footer.py
@@ -6,7 +6,7 @@ def render_footer():
         """
         <div style="text-align:center; color:#999; font-size:13px; padding:16px 0;">
             이어질 숙명 · 숙명여자대학교 멘토·멘티 매칭 서비스<br>
-            © 2026 Sookmyung Mentoring Team. All rights reserved.<br>
+            © 2026 숙명여자대학교 자기주도 동아리 'NOVA'. All rights reserved.<br>
         </div>
         """,
         unsafe_allow_html=True,
