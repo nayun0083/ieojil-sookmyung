4c1492eba8bbcaec732b59b869afc6b69110392a
nayun0083 <luzdelsolnayun@gmail.com>
2026-08-25 15:05:49 +0900

    Remove contact email from footer
    Removed contact email from footer.

--- components/footer.py
+++ components/footer.py
@@ -7,7 +7,6 @@ def render_footer():
         <div style="text-align:center; color:#999; font-size:13px; padding:16px 0;">
             이어질 숙명 · 숙명여자대학교 멘토·멘티 매칭 서비스<br>
             © 2026 Sookmyung Mentoring Team. All rights reserved.<br>
-            문의: mentoring@sookmyung.ac.kr
         </div>
         """,
         unsafe_allow_html=True,
